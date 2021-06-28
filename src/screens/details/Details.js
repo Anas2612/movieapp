d30f256b661ca74a5e21c5b67b7ca305738a6519
anas <maali@falabella.cl>
2021-06-28 12:39:33 +0530

    initial code , Header and Home Page completed

---
+++ src/screens/details/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../../common/header/Header";
+
+
+const Details = (props) => {
+    return (
+        <div>
+            <Header showBookShowButton={true} {...props}></Header>
+        </div>
+    )
+}
